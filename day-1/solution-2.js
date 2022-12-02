ba34df8b2248476e770e19afdea0f25f3ad1e92e
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-12-02 08:25:28 -0700

    blah

--- day-1/solution-2.js
+++ day-1/solution-2.js
@@ -1,16 +1,5 @@
 const { input } = require('./input');
 
-// const sumsSorted = input
-// 	.map((elf) => {
-// 		return elf
-// 			.split('\n')
-// 			.map((item) => parseInt(item, 10))
-// 			.reduce((sum, v) => sum + v, 0);
-// 	})
-// 	.sort((a, z) => z - a);
-
-// console.log(sumsSorted.slice(0, 3).reduce((sum, v) => sum + v, 0));
-
 const sorted = input
 	.map((elf) => {
 		return elf.split('\n').map((collection) => parseInt(collection)).reduce((sum, v) => sum + v)
